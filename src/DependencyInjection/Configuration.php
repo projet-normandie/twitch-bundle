59326234777fd07344e607943d2dd77fc5709a22
magicbart <magicbart@gmail.com>
2022-11-18 11:16:25 +0100

    Initial commit

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ProjetNormandie\TwitchBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('projet_normandie_twitch');
+        $rootnode = $treeBuilder->getRootNode();
+        return $treeBuilder;
+    }
+}
---
+++ src/Entity/Twitch.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ProjetNormandie\TwitchBundle\Entity;
+
+class Twitch
+{
+    /**
+     * @var int|null
+     */
+    private ?int $id = null;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('%d', $this->getId());
+    }
+
+
+    /**
+     * Set id
+     * @param integer $id
+     * @return Twitch
+     */
+    public function setId(int $id): Twitch
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Get id
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
