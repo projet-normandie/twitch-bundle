99f6ae6e1d9d0af27364695dcb15d3bb352e6137
magicbart <magicbart@gmail.com>
2024-10-15 22:02:26 +0200

    Add api games

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -4,31 +4,59 @@ declare(strict_types=1);
 
 namespace ProjetNormandie\TwitchBundle\Entity;
 
+
+use ApiPlatform\Doctrine\Orm\Filter\ExistsFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use ProjetNormandie\TwitchBundle\Repository\GameRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Table(name:'pnt_game')]
 #[ORM\Entity(repositoryClass: GameRepository::class)]
+#[ApiResource(
+    routePrefix: '/twitch',
+    operations: [
+        new GetCollection(),
+        new Get()
+    ],
+    normalizationContext: ['groups' => ['game:read']]
+)]
+#[ApiFilter(
+    OrderFilter::class,
+    properties: [
+        'lastStreamAt' => 'DESC',
+    ]
+)]
+#[ApiFilter(ExistsFilter::class, properties: ['picture'])]
 class Game
 {
     #[ApiProperty(identifier: true)]
+    #[Groups(['game:read'])]
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     protected ?int $id = null;
 
     #[ORM\Column(length: 20, unique: true)]
     protected string $externalId;
 
+    #[Groups(['game:read'])]
     #[ORM\Column(length: 255, nullable: false)]
     private string $name = '';
 
+    #[Groups(['game:read'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $picture = null;
 
+    #[Groups(['game:read'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $url = null;
 
+    #[Groups(['game:read'])]
     #[ORM\Column(nullable: false)]
     private DateTime $lastStreamAt;
 
